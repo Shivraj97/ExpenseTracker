286161908ead50321c12f01f5bdd53b1e7cd0eca
Shivraj97 <43899907+Shivraj97@users.noreply.github.com>
2020-05-30 21:10:22 +0530

    Expense Tracker build using MERN

---
+++ server.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const express = require("express");
+const dotenv = require("dotenv");
+const colors = require("colors");
+const morgan = require("morgan");
+const connectDB = require("./config/db");
+
+dotenv.config({ path: "./config/config.env" });
+
+// Connect to DB
+connectDB();
+
+const transactions = require("./routes/transactions");
+
+const app = express();
+
+// Body Parser
+app.use(express.json());
+
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
+}
+
+app.use("/api/v1/transactions", transactions);
+
+// Serve static assets if in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"))
+  );
+}
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} mode on port ${PORT}`.yellow.bold
+  )
+);
